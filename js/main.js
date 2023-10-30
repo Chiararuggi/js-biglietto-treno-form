cee114118168543f3aad75aeff4102082db46037
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-10-30 11:17:10 +0100

    first if

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-let nameSurname = getElementById(nameSurname);
-let kmNeeded = getElementById(kmNeeded);
-let age = getElementById(age);
+let nameSurnameInput = document.getElementById("nameSurname");
+let kmNeededInput = document.getElementById("kmNeeded");
+let ageInput = document.getElementById("age");
+
+
+document.getElementById("generatesButton").addEventListener("click", function(){
+    let age = ageInput.value;
+    let kmNeeded = kmNeededInput.value;
+    const kmPrice = 0.21;
+    let ticketPrice = kmNeeded * kmPrice;
+    
+
+});

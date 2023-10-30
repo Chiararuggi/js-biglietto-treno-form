8af9df473b13adc8c64ade14d8b77a924058c594
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-10-30 11:20:49 +0100

    second if

--- js/main.js
+++ js/main.js
@@ -8,6 +8,15 @@ document.getElementById("generatesButton").addEventListener("click", function(){
     let kmNeeded = kmNeededInput.value;
     const kmPrice = 0.21;
     let ticketPrice = kmNeeded * kmPrice;
-    
+    console.log(ticketPrice)
 
+    if (age >= 65){
+        const newPrice= ticketPrice - (ticketPrice * 0.4);
+        console.log(newPrice)
+    }
+
+    if (age < 18){
+        const newPrice= ticketPrice - (ticketPrice * 0.2);
+        console.log(newPrice)
+    }
 });
